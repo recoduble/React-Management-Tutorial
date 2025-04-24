89d114df552dc9b5a3fdf5792194f297f05d7768
recoduble <drag12@hanmail.net>
2025-04-24 13:32:33 +0900

    Create Cusomer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
+import { Component } from 'react';
+import Customer from './components/Customer';
+
 import logo from './logo.svg';
 import './App.css';
+const customers = [
+  {
+    'id':1,
+    //'image':'https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill',
+    //'image':'https://picsum.photos/64/64',
+    //'image':'https://loremflickr.com/120/120?random=1',
+    'image':'https://via.assets.so/movie.png?id=1&q=95&w=120&h=120&fit=fill',
+    'name' : '나동빈',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id':2,
+    'image':'https://via.assets.so/movie.png?id=2&q=95&w=120&h=120&fit=fill',
+    'name' : '홍길동',
+    'birthday' : '960305',
+    'gender' : '남자',
+    'job' : '프로그래머'  
+  },
+  {
+    'id':3,
+    'image':'https://via.assets.so/movie.png?id=3&q=95&w=120&h=120&fit=fill',
+    'name' : '홍길동3',
+    'birthday' : '960305',
+    'gender' : '남자',
+    'job' : '프로그래머3'  
+  }  
+]
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-        
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        }) }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <CustomerProfile image={this.props.image} name={this.props.name} id={this.props.id} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            </div>
+        );
+    }
+}
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name} ( {this.props.id} )</h2>
+            </div>
+        );
+    }
+}
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.birthday}</h2>
+                <h2>{this.props.gender}</h2>
+                <h2>{this.props.job}</h2>
+            </div>
+        );
+    }
+
+}
+export default Customer;
