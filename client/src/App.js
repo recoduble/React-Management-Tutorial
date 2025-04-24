72d3aa5f27943f3b505053ef09f1916447a2c61f
recoduble <drag12@hanmail.net>
2025-04-24 16:34:32 +0900

    add get customer REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import Customer from './components/Customer';
 
-import logo from './logo.svg';
 import './App.css';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -12,68 +11,30 @@ import TableCell from '@mui/material/TableCell';
 //import { withStyles } from '@mui/styles';
 //import { withStyles } from '@mui/material/styles';
 //import { styled } from '@mui/material/styles'; //  이거 대신 sx 사용. sx는 MUI(Material-UI)에서 제공하는 스타일링 전용 prop입니다
-
 // npm install @mui/styles
 // npm install @mui/material @emotion/react @emotion/styled
 
-// const styles = theme => ({
-//   root: {
-//     width: "100%",
-//     //marginTop: theme.spacing.unit *3,
-//     marginTop: theme.spacing(3),
-//     overflowX:"auto"
-//   },
-//   table:{
-//     minWidth:1080
-//   }
-// })
-
-// const StyledPaper = styled(Paper)(({ theme }) => ({
-//   width: "100%",
-//   marginTop: theme.spacing(3),
-//   overflowX: "auto"
-// }));
-
-// const StyledTable = styled(Table)({
-//   minWidth: 1080
-// });
-
-const customers = [
-  {
-    'id':1,
-    //'image':'https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill',
-    'image':'https://picsum.photos/128/128?random=1',
-    //'image':'https://loremflickr.com/120/120?random=1',
-    //'image':'https://via.assets.so/movie.png?id=1&q=95&w=120&h=120&fit=fill',
-    'name' : '나동빈',
-    'birthday' : '961222',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {
-    'id':2,
-    'image':'https://picsum.photos/128/128?random=2',
-    //'image':'https://via.assets.so/movie.png?id=2&q=95&w=120&h=120&fit=fill',
-    'name' : '홍길동',
-    'birthday' : '960305',
-    'gender' : '남자',
-    'job' : '프로그래머'  
-  },
-  {
-    'id':3,
-    'image':'https://picsum.photos/128/128?random=3',
-    //'image':'https://via.assets.so/movie.png?id=3&q=95&w=120&h=120&fit=fill',
-    'name' : '홍길동3',
-    'birthday' : '960305',
-    'gender' : '남자',
-    'job' : '프로그래머3'  
-  }  
-]
+//const customers = []
 
 class App extends Component {
+  state = {
+    customers:""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
-
     return (
       // <Paper className={classes.root}>
       <Paper sx={{ width: "100%", mt: 3, overflowX: "auto" }}>
@@ -91,9 +52,9 @@ class App extends Component {
           </TableHead>
           <TableBody>
           
-            { customers.map(c => {
+            { this.state.customers ? this.state.customers.map(c => {
               return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}  /> );
-            }) }
+            }) : "" }
           
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -7,6 +7,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/hello', (req, res) => {
-    res.send({message: 'hello Express!'});
+    res.send({message: 'hello Express!!!!'});
 })
+
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':1,
+            'image':'https://picsum.photos/128/128?random=1',
+            'name' : '나동빈',
+            'birthday' : '961222',
+            'gender' : '남자',
+            'job' : '대학생'
+          },
+          {
+            'id':2,
+            'image':'https://picsum.photos/128/128?random=2',
+            'name' : '홍길동',
+            'birthday' : '960305',
+            'gender' : '남자',
+            'job' : '프로그래머'  
+          },
+          {
+            'id':3,
+            'image':'https://picsum.photos/128/128?random=3',
+            'name' : 'John',
+            'birthday' : '960306',
+            'gender' : '남자',
+            'job' : '프로그래머3'  
+          }  
+        
+    ]);
+})
+
 app.listen(port, () => console.log(`Listening on  port ${port}`));
