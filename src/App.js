b40b7725908e34e2e4e5e04e3277dce74674183e
recoduble <drag12@hanmail.net>
2025-04-24 14:54:29 +0900

    MUI 5

--- src/App.js
+++ src/App.js
@@ -3,13 +3,48 @@ import Customer from './components/Customer';
 
 import logo from './logo.svg';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+//import { withStyles } from '@mui/styles';
+//import { withStyles } from '@mui/material/styles';
+//import { styled } from '@mui/material/styles'; //  이거 대신 sx 사용. sx는 MUI(Material-UI)에서 제공하는 스타일링 전용 prop입니다
+
+// npm install @mui/styles
+// npm install @mui/material @emotion/react @emotion/styled
+
+// const styles = theme => ({
+//   root: {
+//     width: "100%",
+//     //marginTop: theme.spacing.unit *3,
+//     marginTop: theme.spacing(3),
+//     overflowX:"auto"
+//   },
+//   table:{
+//     minWidth:1080
+//   }
+// })
+
+// const StyledPaper = styled(Paper)(({ theme }) => ({
+//   width: "100%",
+//   marginTop: theme.spacing(3),
+//   overflowX: "auto"
+// }));
+
+// const StyledTable = styled(Table)({
+//   minWidth: 1080
+// });
+
 const customers = [
   {
     'id':1,
     //'image':'https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill',
-    //'image':'https://picsum.photos/64/64',
+    'image':'https://picsum.photos/128/128?random=1',
     //'image':'https://loremflickr.com/120/120?random=1',
-    'image':'https://via.assets.so/movie.png?id=1&q=95&w=120&h=120&fit=fill',
+    //'image':'https://via.assets.so/movie.png?id=1&q=95&w=120&h=120&fit=fill',
     'name' : '나동빈',
     'birthday' : '961222',
     'gender' : '남자',
@@ -17,7 +52,8 @@ const customers = [
   },
   {
     'id':2,
-    'image':'https://via.assets.so/movie.png?id=2&q=95&w=120&h=120&fit=fill',
+    'image':'https://picsum.photos/128/128?random=2',
+    //'image':'https://via.assets.so/movie.png?id=2&q=95&w=120&h=120&fit=fill',
     'name' : '홍길동',
     'birthday' : '960305',
     'gender' : '남자',
@@ -25,7 +61,8 @@ const customers = [
   },
   {
     'id':3,
-    'image':'https://via.assets.so/movie.png?id=3&q=95&w=120&h=120&fit=fill',
+    'image':'https://picsum.photos/128/128?random=3',
+    //'image':'https://via.assets.so/movie.png?id=3&q=95&w=120&h=120&fit=fill',
     'name' : '홍길동3',
     'birthday' : '960305',
     'gender' : '남자',
@@ -35,24 +72,35 @@ const customers = [
 
 class App extends Component {
   render() {
+    const {classes} = this.props;
+
     return (
-      <div>
-        { customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        }) }
-      </div>
+      // <Paper className={classes.root}>
+      <Paper sx={{ width: "100%", mt: 3, overflowX: "auto" }}>
+        {/* <Table className={classes.table}> */}
+        <Table sx={{ minWidth: 1080 }}>
+          <TableHead>          
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          
+            { customers.map(c => {
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}  /> );
+            }) }
+          
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
+//export default withStyles(styles)(App);
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,35 +1,49 @@
 import React from 'react';
 
+// npm install @material-ui/core 안됨.
+// npm install @mui/material @emotion/react @emotion/styled
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <CustomerProfile image={this.props.image} name={this.props.name} id={this.props.id} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-            </div>
-        );
+            // <div>
+            //     <CustomerProfile image={this.props.image} name={this.props.name} id={this.props.id} />
+            //     <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            // </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        )
     }
 }
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name} ( {this.props.id} )</h2>
-            </div>
-        );
-    }
-}
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>{this.props.birthday}</h2>
-                <h2>{this.props.gender}</h2>
-                <h2>{this.props.job}</h2>
-            </div>
-        );
-    }
 
-}
+// class CustomerProfile extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <img src={this.props.image} alt="profile" />
+//                 <h2>{this.props.name} ( {this.props.id} )</h2>
+//             </div>
+//         );
+//     }
+// }
+// class CustomerInfo extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>{this.props.birthday}</h2>
+//                 <h2>{this.props.gender}</h2>
+//                 <h2>{this.props.job}</h2>
+//             </div>
+//         );
+//     }
+
+// }
 export default Customer;
